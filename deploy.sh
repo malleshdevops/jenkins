1c1b95fe54233fd0ad4ac0878c158030bdcf322c
Mallesh <malleshdevops2021@gmail.com>
2022-06-03 18:58:37 +0000

    added helm code

---
+++ deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+CDIR=$(cd `dirname "$0"` && pwd)
+cd "$CDIR"
+set -e
+function help()
+{
+  echo '''
+    usage: bash deploy.sh <Namespace> <IMAGE_TAG>
+	'''
+}
+
+NAMESPACE=$1
+CHARTNAME=$1
+IMAGE_TAG=$2
+
+VALUE_FILE=./mychart/values-${NAMESPACE}.yaml
+
+function deploy()
+{
+ case ${NAMESPACE} in
+       dev-httpd|test-httpd|prod-httpd)
+	       helm upgrade --install --values ${VALUE_FILE} $CHARTNAME ./mychart \
+		   --namespace ${NAMESPACE} 
+		 #  --set httpd.image.tag=${IMAGE_TAG}
+		;;
+ esac
+ }
+ 
+if [ ! $# == 2 ]
+then
+  help
+  exit 0
+else
+   deploy "$@"
+   deployName=${NAMESPACE}
+fi
